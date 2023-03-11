11dc671bccc5b60685bf270c94bc5c0c878873d5
Daksh-raj <medhavisharma300@gmail.com>
2023-03-11 15:20:28 +0530

    Added the black scholes formula

--- myapp/views.py
+++ myapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,HttpResponse
 from datetime import datetime
 from myapp.models import Contact
 import math
+from scipy.stats import norm
 # Create your views here.
 def index(request):
     context={
@@ -95,41 +96,21 @@ def black_scholes(request):
         type=request.POST.get("type")
         rate=request.POST.get("rate")
         timeperiod=request.POST.get("timeperiod")
-        binomial=request.POST.get("binomial")
         sigma=request.POST.get("sigma")
-        u=math.exp(float(sigma)*math.sqrt(float(timeperiod)/float(binomial)))
-        d=math.exp(-float(sigma)*math.sqrt(float(timeperiod)/float(binomial)))
-        pu=((math.exp(float(rate)*float(timeperiod)/float(binomial)))-float(d))/(float(u)-float(d))
-        pd=1-pu
-        disc=math.exp(-float(rate)*float(timeperiod)/float(binomial))
-
-        St = [0] * (int(binomial)+1)
-        C = [0] * (int(binomial)+1)
-    
-        St[0]=float(current)*float(d)**int(binomial)
-    
-        for j in range(1, int(binomial)+1): 
-            St[j] = St[j-1] * u/d
-    
-        for j in range(1, int(binomial)+1):
-            if type == 'put':
-                C[j] = max(float(strike)-St[j],0)
-            elif type == 'call':
-                C[j] = max(St[j]-float(strike),0)
-    
-        for i in range(int(binomial), 0, -1):
-            for j in range(0, i):
-                C[j] = disc*(pu*C[j+1]+pd*C[j])
-            
+        d1=(math.log(float(current)/float(strike))+(float(rate)+float(sigma)*float(sigma))*float(timeperiod))/float(sigma)*math.sqrt(float(timeperiod))
+        d2=d1-float(sigma)*math.sqrt(float(timeperiod))
+        price=0
+        if(type=="call"):
+            price=norm.cdf(float(d1), 0, float(sigma)*float(sigma))*float(current)-norm.cdf(d2, 0, float(sigma)*float(sigma))*float(strike)*math.exp(-float(rate)*float(timeperiod))
+        else:
+            price=float(current)*math.exp(-float(rate)*float(timeperiod))*norm.cdf(-d2,0,float(sigma)*float(sigma))-float(current)*norm.cdf(-d1,0,float(sigma)*float(sigma))
         ob.update({"type":type})
         ob.update({"rate":rate})
-        ob.update({"upfactor":u})
-        ob.update({"downfactor":d})
         ob.update({"timeperiod":timeperiod})
         ob.update({"strike":strike})
         ob.update({"current":current})
         ob.update({"volatility":sigma})
-        ob.update({"optionPrice":C[0]})
+        ob.update({"optionPrice":price})
     return render(request,"black_scholes.html",ob)
 def contact(request):
     ob={}
